b811cfe393085fc0f2aa7ab8482bed563ea1b2e3
zeshan <zeshansahi4u@gmail.com>
2023-09-13 02:51:00 -0700

    Initial commit

---
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -0,0 +1,35 @@
+    package com.example.testing;
+
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.text.TextUtils;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.TextView;
+    import android.widget.Toast;
+
+    import androidx.annotation.NonNull;
+    import androidx.appcompat.app.AppCompatActivity;
+
+    import com.google.android.gms.tasks.OnCompleteListener;
+    import com.google.android.gms.tasks.Task;
+    import com.google.firebase.auth.AuthResult;
+    import com.google.firebase.auth.FirebaseAuth;
+    import com.google.firebase.database.DataSnapshot;
+    import com.google.firebase.database.DatabaseError;
+    import com.google.firebase.database.DatabaseReference;
+    import com.google.firebase.database.FirebaseDatabase;
+    import com.google.firebase.database.ValueEventListener;
+
+    public class MainActivity extends AppCompatActivity {
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+        }
+
+    }
