c43c8e53513b19a603e3c63bf4e9755272ea4a67
zeshan <zeshansahi4u@gmail.com>
2023-09-13 02:55:08 -0700

    second commit

--- app/src/main/java/com/example/testing/MainActivity.java
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -23,13 +23,10 @@
     import com.google.firebase.database.ValueEventListener;
 
     public class MainActivity extends AppCompatActivity {
-
-
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
         }
-
     }
